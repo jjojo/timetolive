64f9aa636a14e0ca1557883284b0598123203891
Jonas <jan.jonas.johansson@gmail.com>
2022-09-26 23:31:05 +0200

    hollow rendering solved

--- src/components/LifeModel/LifeModel.js
+++ src/components/LifeModel/LifeModel.js
@@ -1,13 +1,42 @@
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
+import { beforeCompileShader } from "../../helpers/beforeCompileShader";
+import { createInstancedBufferGeometry } from "../../helpers/createInstancedBufferGeometry";
+import { SpheresBlock } from "../SpheresBlock/SpheresBlock";
 
 const dateOfBirth = new Date(1992, 6, 9);
 
-const daysLived = Math.round(
+let daysLived = Math.round(
   new Date(new Date().getTime() - dateOfBirth.getTime()) / (1000 * 60 * 60 * 24)
 );
 
-console.log(daysLived);
+const yearsLived = Math.round(
+  new Date(new Date().getTime() - dateOfBirth.getTime()) /
+    (1000 * 60 * 60 * 24) /
+    365
+);
+
+const weeksLived = Math.round(
+  new Date(new Date().getTime() - dateOfBirth.getTime()) /
+    (1000 * 60 * 60 * 24) /
+    7
+);
+
+const hoursLived = Math.round(
+  new Date(new Date().getTime() - dateOfBirth.getTime()) / (1000 * 60 * 60)
+);
+
+const minutesLived = Math.round(
+  new Date(new Date().getTime() - dateOfBirth.getTime()) / (1000 * 60)
+);
+
+const secondsLived = Math.round(
+  new Date(new Date().getTime() - dateOfBirth.getTime()) / (1000 * 12)
+);
+
+// daysLived = yearsLived;
+
+// console.log(yearsLived);
 
 const sphereTexture = new THREE.TextureLoader().load("/ball.png", (t) => {
   t.center.setScalar(0.5);
@@ -20,147 +49,36 @@ const currentPointMat = new THREE.PointsMaterial({
   alphaTest: 0.3,
   transparent: true,
 });
-const pointsMatTransparent = new THREE.PointsMaterial({
-  map: sphereTexture,
-  vertexColors: true,
-  alphaTest: 0.1,
-  transparent: true,
-  opacity: 0.3,
-});
-const pointsMat = new THREE.PointsMaterial({
-  map: sphereTexture,
-  vertexColors: true,
-  alphaTest: 0.5,
-  transparent: false,
-});
 
-const beforeCompileShader = (shader) => {
-  shader.vertexShader =
-    `
-  	attribute float sizes;
-    attribute vec3 offset;
-  ` + shader.vertexShader;
-
-  shader.vertexShader = shader.vertexShader.replace(
-    `#include <begin_vertex>`,
-    `#include <begin_vertex>
-    transformed += offset;
-    `
-  );
-  shader.vertexShader = shader.vertexShader.replace(
-    `gl_PointSize = size;`,
-    `gl_PointSize = size * sizes;`
-  );
-};
-
-pointsMat.onBeforeCompile = beforeCompileShader;
-pointsMatTransparent.onBeforeCompile = beforeCompileShader;
 currentPointMat.onBeforeCompile = beforeCompileShader;
 
 const radius = 11;
-const createPosSetter =
-  (n, r = radius) =>
-  (array, i, x, y, z) => {
-    array[i * 3] = (n * r) / 2 - x * r;
-    array[i * 3 + 1] = (n * r) / 2 - y * r;
-    array[i * 3 + 2] = (n * r) / 2 - z * r;
-    return array;
-  };
-
-const createGeometry = ({ positions, colors, sizes }) => {
-  const geometry = new THREE.InstancedBufferGeometry();
-  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
-  geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
-  geometry.setAttribute("sizes", new THREE.BufferAttribute(sizes, 1));
-  const sumDisp = new Float32Array([0, 0, 0]);
-  geometry.setAttribute(
-    "offset",
-    new THREE.InstancedBufferAttribute(sumDisp, 3)
-  );
-  return geometry;
-};
 
 export const LifeModel = ({ amount, count }) => {
-  const setPos = createPosSetter(amount);
-
-  const transparentSpheresPos = new Float32Array(count * 3);
-  let positions = [];
   const currentPosition = new Float32Array(3);
-  const colors = new Float32Array(count * 3);
-  const transparentColor = new Float32Array(count * 3).fill(1);
   const currentColor = new Float32Array([1, 0, 0]);
   const sizes = new Float32Array(count).fill(28);
-  const spherePadding = 5;
   let i = 0;
+
   for (let x = 0; x < amount; x++) {
+    if (i > daysLived) break;
     for (let y = 0; y < amount; y++) {
+      if (i > daysLived) break;
       for (let z = 0; z < amount; z++) {
-        if (i > daysLived) {
-          if (
-            !(
-              i >
-                (amount - spherePadding) * amount * 2 +
-                  spherePadding +
-                  daysLived &&
-              i < count - amount * amount * spherePadding + 1 &&
-              i % amount > spherePadding - 1 &&
-              (i + spherePadding) % amount > spherePadding - 1 &&
-              i % (amount * amount) > amount * spherePadding &&
-              (i + amount * spherePadding) % (amount * amount) > amount * 2
-            )
-          ) {
-            // setPos(positions, i, x, y, z);
-            positions.push(
-              (amount * 11) / 2 - x * 11,
-              (amount * 11) / 2 - y * 11,
-              (amount * 11) / 2 - z * 11
-            );
-          }
-        } else if (i === daysLived) {
+        if (i > daysLived) break;
+        if (i === daysLived) {
           currentPosition[0] = (amount * radius) / 2 - x * radius;
           currentPosition[1] = (amount * radius) / 2 - y * radius;
           currentPosition[2] = (amount * radius) / 2 - z * radius;
-        } else {
-          setPos(transparentSpheresPos, i, x, y, z);
-        }
-
-        if (
-          i >
-            (amount - spherePadding) * amount * 2 + spherePadding + daysLived &&
-          i < count - amount * amount * spherePadding + 1 &&
-          i % amount > spherePadding - 1 &&
-          (i + spherePadding) % amount > spherePadding - 1 &&
-          i % (amount * amount) > amount * spherePadding &&
-          (i + amount * spherePadding) % (amount * amount) > amount * 2
-        ) {
-          colors[i * 3 + 0] = Math.random();
-          colors[i * 3 + 1] = Math.random();
-          colors[i * 3 + 2] = Math.random();
-        } else {
-          colors[i * 3 + 0] = 1;
-          colors[i * 3 + 1] = 1;
-          colors[i * 3 + 2] = 1;
         }
         i++;
       }
     }
   }
 
-  const currentGeometry = createGeometry({
+  const currentGeometry = createInstancedBufferGeometry({
     positions: currentPosition,
     colors: currentColor,
-    sizes,
-  });
-  console.log(positions);
-  const geometry = createGeometry({
-    positions: new Float32Array(positions),
-    colors: colors,
-    sizes,
-  });
-
-  const transparentGeometry = createGeometry({
-    positions: transparentSpheresPos,
-    colors: transparentColor,
     sizes,
   });
 
@@ -173,8 +91,13 @@ export const LifeModel = ({ amount, count }) => {
   return (
     <>
       <points args={[currentGeometry, currentPointMat]} />
-      <points args={[transparentGeometry, pointsMatTransparent]} />
-      <points args={[geometry, pointsMat]} />
+      <SpheresBlock
+        count={count}
+        amount={amount}
+        timeLived={daysLived}
+        wallThickness={2}
+        sphereRadius={11}
+      />
     </>
   );
 };
--- src/components/Model3D/Model3D.js
+++ src/components/Model3D/Model3D.js
@@ -6,8 +6,13 @@ import { useState } from "react";
 // const AMOUNT = 100;
 // const COUNT = Math.pow(AMOUNT, 3);
 
+// amount 438 => 80 years in seconds
+// amount 31 => 81.6 years in days
+// amount 16 => 80 years in weeks
+// amount 4 => 80 years in years
+
 export const Model3D = () => {
-  const [amount, setAmount] = useState(35);
+  const [amount, setAmount] = useState(31);
   const [count, setCount] = useState(Math.pow(amount, 3));
 
   return (
@@ -16,8 +21,8 @@ export const Model3D = () => {
       camera={{
         fov: 50,
         aspect: window.innerWidth / window.innerHeight,
-        near: 2,
-        far: 5000,
+        near: 1,
+        far: Math.max(amount * amount * amount, 1000),
         position: [amount * 12, amount * 12, amount * 12],
       }}
     >
